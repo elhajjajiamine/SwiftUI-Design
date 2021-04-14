76bbf006cc8f9f51279647043f7d6ace580e640c
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-14 13:26:25 +0000

    Initial Commit

---
+++ SwiftUI Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Design
+//
+//  Created by elhajjaji on 14/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Design/SwiftUI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignApp.swift
+//  SwiftUI Design
+//
+//  Created by elhajjaji on 14/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
